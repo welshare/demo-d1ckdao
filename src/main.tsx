334c0cb2da434c4b49845f53f42b1007175a663d
Stefan Adolf <stadolf@gmail.com>
2025-10-03 22:31:26 +0200

    uses Jost as font
    Signed-off-by: Stefan Adolf <stadolf@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/jost/400.css'
+import '@fontsource/jost/600.css'
+import '@fontsource/jost/700.css'
 import './index.css'
 import App from './App.tsx'
 
